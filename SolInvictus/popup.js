9520c92174e69f4abd03ddc6ea4910526147cdf8
Ricky Blondel <rickyblondel@icloud.com>
2018-05-10 15:14:59 +0400

    V0.2.2.100520181514
    Search Url + sitemap + viewsource done
    left => init list of all market...

--- SolInvictus/popup.js
+++ SolInvictus/popup.js
@@ -42,13 +42,32 @@ btnConfirm.onclick=function(activeTab){
 }
 
 /* */
-const listUrl=()=>{
-    return 'https://www.pampers.fr';
+const initListOfUrls=(arg)=>{
+    try{
+        let listMarketsCD=[['fr','https://www.pampers.fr'],['us_en','https://www.pampers.com']];
+        let listMarketsCM=[['fr','https://author.pampers.fr.pgsitecore.com'],['us_en','https://author.pampers.com.pgsitecore.com']];
+        if(arg=='cd')return listMarketsCD;
+        if(arg=='cm')return listMarketsCM;
+    }
+    catch(e){
+        console.log('Error initListOfUrls');
+    }
+}
+
+const searchMarket=(argList,argMarket)=>{
+    let result='';
+    for(var compt=0;compt<argList.length;compt++){
+        if(argList[compt][0]==argMarket){
+            result=argList[compt][1];
+            break;
+        }
+    }
+    return result;
 }
 
 const urlMarket=(argMarket,argEnvironment)=>{
-    console.log(argMarket+" "+argEnvironment);
-    let listOfUrls=listUrl();
+    let resultUrl=searchMarket(initListOfUrls(argEnvironment),argMarket);
+    return resultUrl;
 }
 /* */
 
