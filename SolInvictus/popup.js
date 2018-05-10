91f69995a30d6be1d2d4e051687d0b9f8501c5e6
Ricky Blondel <rickyblondel@icloud.com>
2018-05-10 10:23:31 +0400

    v0.1.100520181023
    Mise en place Front

--- SolInvictus/popup.js
+++ SolInvictus/popup.js
@@ -1,50 +1,11 @@
-let htmlFile=document.getElementById('htmlFile');
+let btnConfirm=document.getElementById('btn-confirm');
 
-htmlFile.onchange=function(activeTab){
-    let input = event.target;
-    let reader = new FileReader();
-    let searchKey=document.getElementById('searchKey').value;
-    reader.onload = function(){
-        let dataURL = reader.result;
-        document.getElementById("code-to-scan").innerHTML=dataURL;
-        scanDocument(searchKey);
-    };
-    let result = reader.readAsText(input.files[0]);
-}
-
-const scanDocument=(arg)=> {
-    let totalElements=document.getElementsByClassName(arg).length;
-    let arrayElements="";
-    for(let compteur=0;compteur<totalElements;compteur++){
-        let urlFromHtml=document.getElementsByClassName(arg)[compteur].getAttribute('href');
-        let linkText=document.getElementsByClassName(arg)[compteur].text;
-        let linkDataActionDetail=document.getElementsByClassName(arg)[compteur].getAttribute('data-action-detail');
-        if(urlFromHtml!='javascript:void(0)'){
-            let tempVar="";
-            if(document.getElementById("optionLinkLabel").checked)tempVar=linkText+" | "
-            tempVar=tempVar+urlFromHtml;
-            if(document.getElementById("optionDataActionDetail").checked)tempVar=tempVar+" | "+linkDataActionDetail
-            arrayElements=arrayElements+"<p>"+tempVar+"</p>";
-            if(document.getElementById("optionOpenUrlsOn").checked){
-                //console.log("Option Open Urls ON :"+compteur);
-                openPage(urlFromHtml);
-            }
-            console.log(countElementCompleted(compteur,totalElements));
-        }
-    }
-    console.log('Completed');
-    document.getElementById("total-count").innerHTML=totalElements-1;
-    document.getElementById("list-urls").innerHTML=arrayElements;
-    copyElement(document.getElementById("list-urls"));
-  }
-
-const countElementCompleted=(indice, totalElement)=>{
-    return ((indice*100)/totalElement);
+btnConfirm.onclick=function(activeTab){
+    
 }
 
 const openPage=(urlIn)=>{
     chrome.tabs.create({ url: urlIn , selected: false});
-    //console.log("url || "+urlIn+" || ok");
 }
 
 const copyElement=(el)=>{
